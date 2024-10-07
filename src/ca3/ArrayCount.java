ab180bc73f1fb23dc9032d29c04e05cca280112f
aplbj0722 <aplbj002@mymail.unisa.edu.au>
2024-10-07 18:26:52 +1030

    Completed ArrayCount task
    Array Count class is successfully completed, count method was
    implemented and passes the unit tests.

--- src/ca3/ArrayCount.java
+++ src/ca3/ArrayCount.java
@@ -2,4 +2,13 @@ package ca3;
 
 public class ArrayCount {
 
+    public int count(int[] array, int target) {
+        int count = 0;
+        for (int num : array) {
+            if (num == target) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
