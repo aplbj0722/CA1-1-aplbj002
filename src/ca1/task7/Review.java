442b21227ff5c54d472162674636ab835e31ed3b
aplbj0722 <aplbj002@mymail.unisa.edu.au>
2024-10-07 20:22:24 +1030

    Reviewed task 7 and made relevant changes to the code conversion

--- src/ca1/task7/Review.java
+++ src/ca1/task7/Review.java
@@ -4,7 +4,7 @@ public class Review {
 	    public static void main(String[] args) {
 	    	int[] data = {1, 2, 3, 4, 5};
 	    	
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < data.length; i++) { // changed i < 3 to i < data.length as the original would not loop over the entire array
 				data[i] = data[i] * data[i];
 				
 			}
