8cb48777838b3c902cffca245566e7a34393ef04
aplbj0722 <aplbj002@mymail.unisa.edu.au>
2024-10-07 19:21:31 +1030

    Set up constructor for Car
    private variables are added and constructor is created to initialise
    variables

--- src/ca4/Car.java
+++ src/ca4/Car.java
@@ -1,5 +1,17 @@
 package ca4;
 
+import java.util.Objects;
+
 public class Car {
+		
+    private String make;
+    private String model;
+    private String color;
+    
+	public Car(String make, String model, String color) {
+		this.make = make;
+		this.model = model;
+		this.color = color;
+	}
 
 }
