8327250f3fd2b5e26b3db626514c0c5862487908
aplbj0722 <aplbj002@mymail.unisa.edu.au>
2024-10-07 18:20:01 +1030

    Completed the Loops Task
    Successfully prints "words" the specified amount of times

--- src/ca2/Loops.java
+++ src/ca2/Loops.java
@@ -2,4 +2,14 @@ package ca2;
 
 public class Loops {
 
+    public void printWords(int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println("words");
+        }
+    }
+
+    public static void main(String[] args) {
+        Loops loops = new Loops();
+        loops.printWords(6);
+    }
 }
