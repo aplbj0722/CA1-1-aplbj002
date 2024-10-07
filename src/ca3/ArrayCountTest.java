e0503f35c571cd09d9367187212d9766a276da0a
aplbj0722 <aplbj002@mymail.unisa.edu.au>
2024-10-07 18:23:05 +1030

    Created ArrayCount unit test
    Unit test is created.

--- src/ca3/ArrayCountTest.java
+++ src/ca3/ArrayCountTest.java
@@ -1,14 +1,15 @@
 package ca3;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class ArrayCountTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    void testCount() {
+        ArrayCount arrayCount = new ArrayCount();
+        assertEquals(0, arrayCount.count(new int[]{1, 2, 3, 4, 5}, 6));
+        assertEquals(1, arrayCount.count(new int[]{1, 2, 3, 4, 5}, 2));
+        assertEquals(3, arrayCount.count(new int[]{1, 2, 2, 4, 2}, 2));
+    }
 }
