d4a3c93c08bb56b6198abf26c5276ac5fa4b937d
aplbj0722 <aplbj002@mymail.unisa.edu.au>
2024-10-07 18:12:30 +1030

    Completed Caeser Cypher Task, Test was implemented in initial commit

--- src/ca1/CaeserCypher.java
+++ src/ca1/CaeserCypher.java
@@ -2,4 +2,21 @@ package ca1;
 
 public class CaeserCypher {
 
+    public char[] encode(char[] input, int offset) {
+        char[] result = new char[input.length];
+        for (int i = 0; i < input.length; i++) {
+            char c = input[i];
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                result[i] = (char) ((c - base + offset) % 26 + base);
+            } else {
+                result[i] = c;
+            }
+        }
+        return result;
+    }
+
+    public char[] decode(char[] input, int offset) {
+        return encode(input, 26 - offset);
+    }
 }
--- src/ca1/CaeserCypherTest.java
+++ src/ca1/CaeserCypherTest.java
@@ -3,18 +3,18 @@ package ca1;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class CaesarCypherTest {
+public class CaeserCypherTest {
 
     @Test
     public void testEncode() {
-        CaesarCypher cypher = new CaesarCypher();
+    	CaeserCypher cypher = new CaeserCypher();
         assertArrayEquals(new char[]{'b', 'c', 'd'}, cypher.encode(new char[]{'a', 'b', 'c'}, 1));
         assertArrayEquals(new char[]{'c', 'd', 'e'}, cypher.encode(new char[]{'a', 'b', 'c'}, 2));
     }
 
     @Test
     public void testDecode() {
-        CaesarCypher cypher = new CaesarCypher();
+    	CaeserCypher cypher = new CaeserCypher();
         assertArrayEquals(new char[]{'a', 'b', 'c'}, cypher.decode(new char[]{'b', 'c', 'd'}, 1));
         assertArrayEquals(new char[]{'a', 'b', 'c'}, cypher.decode(new char[]{'c', 'd', 'e'}, 2));
     }
